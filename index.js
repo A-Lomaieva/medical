4a6f87594b213fb5375db46ae6cb4f8121890528
Andrei Sierkov <andrei.sierkov@railsreactor.com>
2016-05-29 22:27:48 +0300

    Testing DB

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+var db;
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL,
+		function(err, client) {
+	if (err) throw err;
+	console.log('Connected to postgres! Getting schemas...');
+	db = client;
+});
+
 app.get('/', function(request, response) {
 	response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/db-test', function(request, response) {
+	db
+			.query('SELECT * FROM patient;')
+			.on('row', function(row) {
+				response.send(
+						`Got data from database:
+						${JSON.stringify(row)}
+						`
+				);
+			});
 });
 
 app.listen(app.get('port'), function() {
