11406bd0a16cb8fb8c8a966124de935859fed2a9
Andrei Sierkov <andrei.sierkov@railsreactor.com>
2016-05-29 23:32:40 +0300

    GET /api/patient

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 var express = require('express');
 var app = express();
 var pg = require('pg');
@@ -8,30 +9,62 @@ app.use(express.static(__dirname + '/public'));
 
 var db;
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL,
-		function(err, client) {
-	if (err) throw err;
-	console.log('Connected to postgres! Getting schemas...');
-	db = client;
-});
+function dbConnect(callback) {
+	pg.defaults.ssl = true;
+	pg.connect(process.env.DATABASE_URL,
+			function (err, client) {
 
-app.get('/', function(request, response) {
+				if (err) throw err;
+				console.log('Connected to postgres!');
+				callback(client);
+			});
+}
+function dbClose(client) {
+	client.end();
+}
+
+
+
+app.get('/', function (request, response) {
 	response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/db-test', function(request, response) {
-	db
-			.query('SELECT * FROM patient;')
-			.on('row', function(row) {
-				response.send(
-						`Got data from database:
-						${JSON.stringify(row)}
-						`
-				);
-			});
+app.get('/api/patient', function (request, response) {
+	var requestQuest = request.query;
+	var results = [];
+
+	dbConnect((client) => {
+		function done() {
+			client.end();
+			response.end();
+		}
+
+		var query = client.query(
+				"SELECT * FROM patient WHERE name ILIKE ($1) AND doctor ILIKE ($2) AND hospital_code ILIKE ($3)",
+				[
+					`%${requestQuest.name || ''}%`,
+					`%${requestQuest.doctor || ''}%`,
+					`%${requestQuest.hospitalCode || ''}%`
+				]);
+
+		query.on('row', function(row) {
+			results.push(row);
+		});
+
+		query.on('end', function() {
+			response.json(results);
+			done();
+		});
+
+	});
 });
 
-app.listen(app.get('port'), function() {
+
+app.get('/db-test', function (request, response) {
+
+
+});
+
+app.listen(app.get('port'), function () {
 	console.log('Node app is running on port', app.get('port'));
 });
