3aff04c5a415e107297d67d60642f16dbbcb3bd2
Andrei Sierkov <andrei.sierkov@railsreactor.com>
2016-05-29 15:04:29 +0300

    Test app

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+var express = require('express');
+var app = express();
 
-app.get('/', (request, response) => {
-	response.send('Lomaieva Medical Team, assemble!')
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+	response.sendFile('public/index.html');
 });
 
-app.listen(port, (err) => {
-	if (err) {
-		return console.log('something bad happened', err)
-	}
-
-	console.log(`server is listening on ${port}`)
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
 });
